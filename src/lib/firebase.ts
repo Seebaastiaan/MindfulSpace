f42e1a198c2999f35d15b838ee2031d24f749624
Seebaastiaan <sebasjara1503@outlook.com>
2025-08-18 18:02:58 -0600

    vercel bugs 2

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -9,6 +9,7 @@ const firebaseConfig = {
   storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
   messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
   appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
+  measurementId: process.env.NEXT_PUBLIC_FIREBASE_MEASUREMENT_ID,
 };
 
 const app = initializeApp(firebaseConfig);
@@ -18,3 +19,6 @@ export const auth = getAuth(app);
 
 // ✅ Siempre retorna un GoogleAuthProvider válido
 export const googleProvider = new GoogleAuthProvider();
+googleProvider.setCustomParameters({
+  client_id: process.env.NEXT_PUBLIC_FIREBASE_CLIENT_ID!,
+});
